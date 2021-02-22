592d1b5ea952bbba34a0b25b49082e5d8e1bb524
Antonio <web.antonio.popa@gmail.com>
2021-02-22 15:08:47 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Corona-Virus-Tracker';
+  background = null;
+
+  // TODO: implements some logic to change background in function of route
+  // ex: if the route is home the background will be covid background
+  // if is cases the background will be blue
+
+  constructor(private router: Router) {
+    if (router.url === "/") {
+      console.log(router.url);
+      this.background = "../../assets/img/home-background.jpg";
+    } else {
+      this.background = "#312C51";
+    }
+   }
+
+  ngOnInit(): void {
+  }
 }
